c553acf7e7a6ec5e45fd27cfb2cc561734a43df6
sameera-insighture <sameeera.l@insighture.com>
2024-01-10 10:15:45 +0530

    fix lint issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 
